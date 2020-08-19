c515e31ff9e6e3a66166580aad9f00d17eaf8121
adaigo <akhr.daigo@gmail.com>
2020-08-19 14:42:22 +0900

    enable to send data with controller

--- src/main/java/com/example/feature/level1/Level1Application.java
+++ src/main/java/com/example/feature/level1/Level1Application.java
@@ -7,21 +7,21 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Level1Application implements CommandLineRunner {
+public class Level1Application /*implements CommandLineRunner*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(Level1Application.class, args);
     }
-
-    private final RestTemplate restTemplate;
-
-    public Level1Application(RestTemplateBuilder restTemplateBuilder){
-        this.restTemplate = restTemplateBuilder.build();
-    }
-
-    @Override
-    public void run(String... args){
-        Quote response = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        System.out.println(response.toString());
-    }
+//
+//    private final RestTemplate restTemplate;
+//
+//    public Level1Application(RestTemplateBuilder restTemplateBuilder){
+//        this.restTemplate = restTemplateBuilder.build();
+//    }
+//
+//    @Override
+//    public void run(String... args){
+//        Quote response = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+//        System.out.println(response.toString());
+//    }
 }
--- src/main/java/com/example/feature/level1/level1Controller.java
+++ src/main/java/com/example/feature/level1/level1Controller.java
@@ -2,21 +2,24 @@ package com.example.feature.level1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class level1Controller {
 
-    @RequestMapping("/index")
-    public String index(){
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index(Model model){
+        model.addAttribute("msg","this is message from controller");
         return "index";
     }
 
-    @RequestMapping("/another")
-    public String another(){
+    @RequestMapping(value = "/another", method = RequestMethod.GET)
+    public String another(Model model){
         return "another";
     }
 
-    @RequestMapping("/other")
+    @RequestMapping(value = "/other", method = RequestMethod.GET)
     public String other(){
         return "other";
     }
