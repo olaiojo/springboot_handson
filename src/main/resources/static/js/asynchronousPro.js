556293947f1ae0ac45d104cd3f6abe1c33cac6e5
olaiojo <akhr.daigo@gmail.com>
2020-09-08 22:04:33 +0900

    feat: create getAPIData() function. when this function is called, send GET request to jsonplaceholder(posts) and output data to console log.

--- src/main/resources/static/js/asynchronousPro.js
+++ src/main/resources/static/js/asynchronousPro.js
@@ -1,3 +1,16 @@
 function jsTest(){
     alert('this is just test.');
 }
+
+const URL = 'https://jsonplaceholder.typicode.com/posts/1';
+
+function getAPIData(){
+    const request = new XMLHttpRequest();
+    request.open('GET', URL, true);
+    request.responseType = 'json';
+    request.onload = function(){
+        const data = this.response;
+        console.log(data);
+    }
+    request.send();
+}
