ca195ea8b6e103219e60f3b26aa3e496f579fbd8
olaiojo <akhr.daigo@gmail.com>
2020-09-09 04:20:00 +0900

    docs: add JSDoc

--- src/main/resources/static/js/asynchronousPro.js
+++ src/main/resources/static/js/asynchronousPro.js
@@ -1,13 +1,25 @@
+/**
+ * JSONPlaceholderのPostsのベースURL
+ * @type {string}
+ */
 const BASE_URL = 'https://jsonplaceholder.typicode.com/posts/';
 
-function jsTest() {
-    alert('this is just test.');
-}
-
+/**
+ * 1~maxまでのランダムな整数を1つ返す関数
+ * @param {number} max ランダムな整数の最大値を指定
+ * @returns {number}
+ */
 function createRandomNumber(max){
     return Math.ceil(Math.random() * max);
 }
 
+/**
+ * other.htmlの該当id部分を引数の値で書き換える関数
+ * @param {number} userId ユーザID
+ * @param {number} id ID
+ * @param {string} title タイトル
+ * @param {string} body 本文
+ */
 function setDataToView(userId, id, title, body) {
     document.getElementById('userId').innerHTML = userId;
     document.getElementById('id').innerHTML = id;
@@ -15,6 +27,9 @@ function setDataToView(userId, id, title, body) {
     document.getElementById('body').innerHTML = body;
 }
 
+/**
+ * JSONPlaceholderのPostsのうち1つの投稿についてGETリクエストを送信し、レスポンスをother.htmlに反映させる関数
+ */
 function getAPIData() {
     const URL = BASE_URL + createRandomNumber(100);
     const request = new XMLHttpRequest();
