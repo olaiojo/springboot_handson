9702c51af863c3589a5aff2e93dd470c3c1c01d0
olaiojo <akhr.daigo@gmail.com>
2020-09-08 22:37:31 +0900

    feat: create setDataToView(). it just sets arguments to view(other.html)

--- src/main/resources/static/js/asynchronousPro.js
+++ src/main/resources/static/js/asynchronousPro.js
@@ -1,14 +1,21 @@
-function jsTest(){
+const URL = 'https://jsonplaceholder.typicode.com/posts/1';
+
+function jsTest() {
     alert('this is just test.');
 }
 
-const URL = 'https://jsonplaceholder.typicode.com/posts/1';
+function setDataToView(userId, id, title, body) {
+    document.getElementById('userId').innerHTML = userId;
+    document.getElementById('id').innerHTML = id;
+    document.getElementById('title').innerHTML = title;
+    document.getElementById('body').innerHTML = body;
+}
 
-function getAPIData(){
+function getAPIData() {
     const request = new XMLHttpRequest();
     request.open('GET', URL, true);
     request.responseType = 'json';
-    request.onload = function(){
+    request.onload = function () {
         const data = this.response;
         console.log(data);
     }
